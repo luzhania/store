dc1be58abdc765504ca9b0901d3898d56ad71cb1
Luzhania Cespedes Arancibia <luzhaniacesar@gmail.com>
2024-05-07 15:18:49 -0400

    Changing products info

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -8,11 +8,11 @@ export class ProductService {
   productList: Product[] = [
     {
       id: 1,
-      title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+      title: "Apple - AirPods with Charging Case (2nd generation) - White",
       price: 109.95,
       description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-      category: "men's clothing",
-      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      category: "Technology article",
+      image: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6084/6084400_sd.jpg;maxHeight=640;maxWidth=550",
       rating: {
         "rate": 3.9,
         "count": 120
@@ -20,11 +20,11 @@ export class ProductService {
     },
     {
       id: 2,
-      title: "Mens Casual Premium Slim Fit T-Shirts ",
-      price: 22.3,
-      description: "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+      title: "MacBook Air 13.6'' Laptop - Apple M2 chip - 8GB Memory - 256GB SSD - Midnight",
+      price: 999,
+      description: "Supercharged by the next-generation M2 chip, the redesigned MacBook Air combines incredible performance and up to 18 hours of battery life into its strikingly thin aluminum enclosure.¹ ",
       category: "men's clothing",
-      image: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+      image: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6509/6509650_sd.jpg;maxHeight=640;maxWidth=550",
       rating: {
         "rate": 4.1,
         "count": 259
@@ -32,11 +32,11 @@ export class ProductService {
     },
     {
       id: 3,
-      title: "Mens Cotton Jacket",
-      price: 55.99,
-      description: "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
-      category: "men's clothing",
-      image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+      title: "Apple Watch Series 9 GPS 41mm Aluminum Case with Starlight Sport Band (Small/Medium) - Starlight",
+      price: 299.00,
+      description: "Apple Watch Series 9 helps you stay connected, active, healthy, and safe. Featuring double tap, a magical way to interact with Apple Watch, an even-brighter display, faster on-device Siri, and Precision Finding for iPhone. ",
+      category: "Technology article",
+      image: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6574/6574321_sd.jpg;maxHeight=640;maxWidth=550",
       rating: {
         "rate": 4.7,
         "count": 500
@@ -44,11 +44,11 @@ export class ProductService {
     },
     {
       id: 4,
-      title: "Mens Casual Slim Fit",
-      price: 15.99,
+      title: "Samsung - Galaxy Z Fold5 256GB - Phantom Black (Verizon)",
+      price: 1300,
       description: "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
       category: "men's clothing",
-      image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+      image: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6548/6548853_sd.jpg;maxHeight=640;maxWidth=550",
       rating: {
         "rate": 2.1,
         "count": 430
@@ -56,11 +56,11 @@ export class ProductService {
     },
     {
       id: 5,
-      title: "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
-      price: 695,
-      description: "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
+      title: "Samsung - 65” Class CU7000 Crystal UHD 4K Smart Tizen TV",
+      price: 399,
+      description: "True-to-life color. Effortless connectivity. Dazzling 4K value. Samsung Crystal UHD is worth a look (and more). Effortlessly access TV shows, movies and ambient content using the Samsung Smart Hub, or find a range of great games on the Samsung Gaming Hub.* Enjoy content even more clear than it was created as its upgraded to 4K resolution ",
       category: "jewelery",
-      image: "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+      image: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6537/6537363_sd.jpg;maxHeight=640;maxWidth=550",
       rating: {
         "rate": 4.6,
         "count": 400
@@ -68,11 +68,11 @@ export class ProductService {
     },
     {
       id: 6,
-      title: "Solid Gold Petite Micropave ",
-      price: 168,
-      description: "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
-      category: "jewelery",
-      image: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+      title: "Samsung - 6'' Class TU690T Crystal UHD 4K Smart Tizen TV",
+      price: 399,
+      description: "See how Crystal UHD with Smart TV elevates what you watch—at a value you’ll love. Go beyond HDTV to enjoy your content in 4K resolution, and in PurColor. With Smart TV powered by Tizen built in, it’s easy to find and stream the latest shows and movies in just a few clicks. ",
+      category: "TV & Home Theater",
+      image: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6538/6538957_sd.jpg;maxHeight=640;maxWidth=550",
       rating: {
         "rate": 3.9,
         "count": 70
@@ -80,11 +80,11 @@ export class ProductService {
     },
     {
       id: 7,
-      title: "White Gold Plated Princess",
-      price: 9.99,
-      description: "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
-      category: "jewelery",
-      image: "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
+      title: "Apple - 10.2-Inch iPad (9th Generation) with Wi-Fi - 64GB - Space Gray",
+      price: 249.99,
+      description: "Powerful. Easy to use. Versatile. The new iPad has a beautiful 10.2-inch Retina display, powerful A13 Bionic chip, an Ultra Wide front camera with Center Stage, and works with Apple Pencil and the Smart Keyboard.¹ iPad lets you do more, more easily. All for an incredible value. ",
+      category: "Computers & Tablets",
+      image: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/4901/4901809_sd.jpg;maxHeight=640;maxWidth=550",
       rating: {
         "rate": 3,
         "count": 400
@@ -92,11 +92,11 @@ export class ProductService {
     },
     {
       id: 8,
-      title: "Pierced Owl Rose Gold Plated Stainless Steel Double",
-      price: 10.99,
-      description: "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
+      title: "Apple - Pencil (1st Generation) with USB-C to Pencil Adapter - White",
+      price: 79.99,
+      description: "Pairing and charging via Lightning Pixel-perfect precision Tilt and pressure sensitivity  Imperceptible lag",
       category: "jewelery",
-      image: "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg",
+      image: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6340/6340401_sd.jpg;maxHeight=640;maxWidth=550",
       rating: {
         "rate": 1.9,
         "count": 100
@@ -104,11 +104,11 @@ export class ProductService {
     },
     {
       id: 9,
-      title: "WD 2TB Elements Portable External Hard Drive - USB 3.0 ",
-      price: 64,
-      description: "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
-      category: "electronics",
-      image: "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
+      title: "Lenovo - Ideapad 1 15.6'' Full HD Touchscreen Laptop - Ryzen 7 5700U with 16GB Memory - AMD Radeon Graphics - 512GB SSD - Cloud Gray",
+      price: 479.99,
+      description: "Browse, explore and connect with confidence on the Lenovo IdeaPad 1 powered by ultra-fast AMD Ryzen 5000 Series Mobile Processors. The IdeaPad 1 packs responsive performance in a thin and compact 17.9 mm chassis, making multitasking on-the-go a breeze. Get the most out of your device with a frameless display for more screen and Dolby Audio speakers, along with 10 hours of battery life and rapid charge. Great for video calls, the 1MP camera comes with a privacy shutter to keep out spying eyes, as well as Smart Noise Cancelling to eliminate background noise. ",
+      category: "Computers & Tablets",
+      image: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6555/6555683_sd.jpg;maxHeight=640;maxWidth=550",
       rating: {
         "rate": 3.3,
         "count": 203
